668e2a1bfdf4c3a9855faaaba49536ce2c895fb9
Kaveen LK <kmklaksitha@gmail.com>
2022-05-08 15:31:00 +0530

    implemented landing page

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -4,21 +4,21 @@ const navbar = () => {
     return (
         <>
             <nav className="navbar">
-                <div className="container-fluid">
+                <div className="container-fluid navbar-container">
                     <h5 className="navbar-brand">
                         {/* <img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"> */}
                         Galadhari - Uganda branch
                     </h5>
-                    <div className="btn-group">
-                        <button className='btn btn-primary'>
+                    <div className="sidetext">
+                        <label className='sidetext-links'>
                             login
-                        </button>
-                        <button className='btn btn-secondary'>
+                        </label>
+                        <label className='sidetext-links'>
                             signup
-                        </button>
+                        </label>
                     </div>
                 </div>
-                <div className="container-fluid">
+                {/* <div className="container-fluid">
                     <div className="nav-bottom">
                         <div className="row nav-bottom-row">
                             <div className="col nav-bottom-col col-4 border-start border-end">
@@ -32,7 +32,7 @@ const navbar = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </nav>
         </>
     )
