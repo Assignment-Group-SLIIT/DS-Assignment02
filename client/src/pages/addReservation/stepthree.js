54ff6baaade65d24090687ef2e16df404150db0f
Kaveen LK <kmklaksitha@gmail.com>
2022-05-08 18:37:45 +0530

    partially created multi step form

---
+++ client/src/pages/addReservation/stepthree.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Stepthree = () => {
+    return (
+        <div>
+            <div className="container step-container">
+            </div>
+        </div>
+    )
+}
+
+export default Stepthree
