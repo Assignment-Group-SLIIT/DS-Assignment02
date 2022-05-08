31eb10642022ccde15dd2b928e94a5c02dd19482
Thisara Thathsarani <thisararpg@gmail.com>
2022-05-08 16:25:18 +0530

    change view hotel details

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,9 @@ import Navbar from './components/navbar';
 import Footer from "./components/footer";
 import Signin from "./pages/signin";
 import Signup from "./pages/signup";
-<<<<<<< HEAD
 import HotelRooms from "./pages/HotelAdmin/viewHotel";
-=======
 import Landingpage from "./pages/landingpage";
 import NotFound from "./pages/notFound";
->>>>>>> ef81da7ade5a4b141691cc626e18ce3449f8fadf
 
 function App() {
 
@@ -36,13 +33,10 @@ function App() {
           <Route path='/' element={<Landingpage />} />
           <Route path='/signin' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
-<<<<<<< HEAD
 
           <Route path='/viewRooms' element={<HotelRooms />} />
-=======
           <Route path='*' element={<NotFound />} />
           {/* <Route path='*' element={<Test />} /> */}
->>>>>>> ef81da7ade5a4b141691cc626e18ce3449f8fadf
         </Routes>
         <Footer />
       </Router>
