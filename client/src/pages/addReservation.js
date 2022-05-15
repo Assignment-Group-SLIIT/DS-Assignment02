6693c40103a0fcb592233aa341de72a8cc5f6d13
Kaveen LK <kmklaksitha@gmail.com>
2022-05-15 11:50:28 +0530

    updated make reservation api call

--- client/src/pages/addReservation.js
+++ client/src/pages/addReservation.js
@@ -111,7 +111,8 @@ const AddReservation = () => {
             amount: reservationRoom.reservationPrice * getDateDiff(),
             CVC: cvv,
             cardHolder: cardOwner,
-            email
+            email,
+            receriverNo: contactNo
         }
 
 
